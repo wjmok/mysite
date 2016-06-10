c3d2240fd86d6daa0ceb8f7d5d65b7c240c9a74f
wjmok <wwjjmm456@gmail.com>
2016-06-10 19:40:34 +0800

    修复注释

--- Application/Admin/Controller/ContentController.class.php
+++ Application/Admin/Controller/ContentController.class.php
@@ -39,13 +39,11 @@ class ContentController extends CommonController
         $this->assign('webSiteMenu', D("Menu")->getBarMenus());
         $this->display();
     }
-    public function add()
-    {
+    
 
-<<<<<<< HEAD
-=======
+
     public function add(){
->>>>>>> c25a4b547f88ca0117f70dd4b1a4371d792f455d
+
         if ($_POST) {
             if (!isset($_POST['title']) || !$_POST['title']) {
                 return show(0, '标题不存在');
