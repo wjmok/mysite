009c199b08fa3680a58384d30aae3bca577e2102
wjmok <wwjjmm456@gmail.com>
2016-06-12 15:40:46 +0800

    完成基本配置模块

--- Public/js/admin/common.js
+++ Public/js/admin/common.js
@@ -43,6 +43,27 @@ $("#singcms-button-submit").click(function() {
         }
     }, 'JSON');
 });
+
+
+$("#bac-submit").click(function() {
+    var data = $("#singcms-form").serializeArray();
+    postData = {};
+    $(data).each(function(i){
+        postData[this.name] = this.value;
+
+    });
+    url = SCOPE.save_url;
+    jump_url = SCOPE.jump_url;
+    $.post(url,postData,function(result){
+        if(result.status == 1) {
+            //成功
+            return dialog.success(result.message,jump_url);
+        }else if(result.status == 0) {
+            // 失败
+            return dialog.error(result.message);
+        }
+    },"JSON");
+});
 /*
 编辑模型
  */
